4efd36050e995e73a395a087a04da256a14e8c29
Leontiy <leosungun@gmail.com>
2023-12-05 17:22:26 +0300

    stable structures

--- sponsor/src/lib.rs
+++ sponsor/src/lib.rs
@@ -1,8 +1,41 @@
 use candid::{CandidType, Deserialize, Principal};
 use ic_cdk::{api::call::ManualReply, query, update};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
+use std::cell::RefCell;
 
-#[query(name = "getSelf", manual_reply = true)]
-fn get_self() -> ManualReply<Principal> {
-    let id = ic_cdk::api::caller();
-    return ManualReply::one(id);
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+thread_local! {
+    // The memory manager is used for simulating multiple memories. Given a `MemoryId` it can
+    // return a memory that can be used by stable structures.
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    // Initialize a `StableBTreeMap` with `MemoryId(0)`.
+    static MAP: RefCell<StableBTreeMap<u128, u128, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
+        )
+    );
 }
+
+// Retrieves the value associated with the given key if it exists.
+#[query(name = "get")]
+fn get(key: u128) -> Option<u128> {
+    MAP.with(|p| p.borrow().get(&key))
+}
+
+// Inserts an entry into the map and returns the previous value of the key if it exists.
+#[update(name = "insert")]
+fn insert(key: u128, value: u128) -> Option<u128> {
+    MAP.with(|p| p.borrow_mut().insert(key, value))
+}
+
+// #[query(name = "getSelf", manual_reply = true)]
+// fn get_self() -> ManualReply<Principal> {
+//     let id = ic_cdk::api::caller();
+//     return ManualReply::one(id);
+// }
+
+ic_cdk::export_candid!();
