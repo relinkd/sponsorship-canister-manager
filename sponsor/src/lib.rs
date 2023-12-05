3f1fb7800d71de28f75d802aaff0fd611645a11b
Leontiy <leosungun@gmail.com>
2023-12-05 14:54:08 +0300

    get principal

--- sponsor/src/lib.rs
+++ sponsor/src/lib.rs
@@ -1,4 +1,8 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Deserialize, Principal};
+use ic_cdk::{api::call::ManualReply, query, update};
+
+#[query(name = "getSelf", manual_reply = true)]
+fn get_self() -> ManualReply<Principal> {
+    let id = ic_cdk::api::caller();
+    return ManualReply::one(id);
 }
