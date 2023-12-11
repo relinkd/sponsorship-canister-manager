89f912fbd9db88ee7dcdf773538b318c79e9fb6d
Leontiy <leosungun@gmail.com>
2023-12-11 13:26:48 +0300

    get self

--- src/test_canister/src/lib.rs
+++ src/test_canister/src/lib.rs
@@ -2,3 +2,10 @@
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+#[ic_cdk::query]
+fn get_self() -> String {
+    let id = ic_cdk::api::caller().to_string();
+
+    return id;
+}
